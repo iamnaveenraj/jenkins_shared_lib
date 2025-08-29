95f4294438841baaa7784892e3ccbb3a3a5296cc
Naveen Kumar <55154827+iamnaveenraj@users.noreply.github.com>
2025-08-29 13:27:26 +0530

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -4,10 +4,10 @@ def call(String project, String ImageTag, String hubUser){
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
+        bat "docker login -u '$USER' -p '$PASS'"
     }
     //sh "docker image push ${hubUser}/${project}:${ImageTag}"
-    sh "docker image push ${hubUser}/${project}:latest"   
+    bat "docker image push ${hubUser}/${project}:latest"   
 }
 
 
